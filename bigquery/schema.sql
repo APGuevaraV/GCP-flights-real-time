99cee2f3f50280e7ed873a9980c8605e3bcddffa
Ana Paula <ana.guevara.valladares@gmail.com>
2025-12-10 11:50:16 -0500

    tidy up files

---
+++ bigquery/schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `name_of_table` (
+  vuelo STRING,
+  aerolinea STRING,
+  fecha_vuelo STRING,
+  origen STRING,
+  destino STRING,
+  latitud STRING,
+  longitud STRING,
+  altitud STRING,
+  velocidad STRING,
+  timestamp STRING
+)
